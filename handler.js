d2c95de024915f4bb40df8207c2c7b813a79ea48
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-19 17:32:29 +0200

    Adjust to new apps dependency

--- handler.js
+++ handler.js
@@ -1,3 +1,11 @@
-export const evaluate = (value) => {
-  return Math.floor(Math.random() * (value.max - value.min + 1)) + value.min;
+import { resolveInput } from "../../public/api.js";
+import config from "@proxtx/config";
+
+export const evaluate = async (value) => {
+  let min = await resolveInput(config.pwd, value.min);
+  return (
+    Math.floor(
+      Math.random() * ((await resolveInput(config.pwd, value.max)) - min + 1)
+    ) + min
+  );
 };
